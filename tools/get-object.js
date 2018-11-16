5640eca55e2f115a0ea9a44c0586b90459ad4924
zhaoyao91 <zhaoyao91@gmail.com>
2018-11-17 03:06:59 +0800

    add get-object

---
+++ tools/get-object.js
@@ -0,0 +1,28 @@
+const axios = require('axios')
+
+const buildUrl = require('./build-url')
+
+module.exports = async function (options) {
+  const {
+    accessToken,
+    baseUrl,
+    apiVersion,
+    objectId,
+    fields,
+  } = options
+
+  const result = await axios({
+    method: 'GET',
+    url: buildUrl({
+      accessToken,
+      baseUrl,
+      apiVersion,
+      resourcePath: objectId,
+      query: {
+        fields: fields.join(',')
+      }
+    }),
+  })
+
+  return result.data
+}
